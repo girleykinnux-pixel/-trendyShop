4142380a5d6685fecadb6d371cb7966e7fdc26fc
yesmith suarez <yesmithsuarez54@gmail.com>
2026-06-19 14:44:21 -0500

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -2,7 +2,7 @@
 const cajaProductos = document.getElementById('contenedor-productos');
 const miPlano = document.getElementById('molde-tarjeta').content; // El molde invisible
 
-// 2. Link de la API para traer los 30 productos principales que quieren tus compañeras
+// 2. Link de la API para traer los 30 productos 
 const apiTienda = 'https://dummyjson.com/products';
 
 // 3. Traer los datos de internet
@@ -18,7 +18,7 @@ fetch(apiTienda)
             // Creamos una copia exacta del molde para este producto
             const copiaTarjeta = miPlano.cloneNode(true);
 
-            // Le metemos los datos reales usando las clases que creamos
+            // Le metemos los datos reales usando las clases 
             copiaTarjeta.querySelector('.etiqueta-descuento').textContent = `${Math.round(item.discountPercentage)}% OFF`;
             copiaTarjeta.querySelector('.imagen-producto').src = item.thumbnail;
             copiaTarjeta.querySelector('.imagen-producto').alt = item.title;
@@ -26,7 +26,7 @@ fetch(apiTienda)
             copiaTarjeta.querySelector('.descripcion-producto').textContent = item.description;
             copiaTarjeta.querySelector('.precio-producto').textContent = `$${item.price} USD`;
 
-            // Colgamos la tarjeta lista en la pantalla
+            // Colgar la tarjeta lista en la pantalla
             cajaProductos.appendChild(copiaTarjeta);
         });
     })
